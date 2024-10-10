c65ee7ad5b61301f6f32c52619e449cadfa855ac
Jyoti <jyotiksajjan1509@gmail.com>
2024-10-10 11:27:48 +0530

    selenium34

---
+++ Amazonwhistlist34.java
@@ -0,0 +1,30 @@
+package selenium_assignment;
+
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazonwhistlist34 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in");
+		driver.manage().window().maximize();
+	WebElement search=driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+	search.sendKeys("shoes"+ Keys.ENTER);
+	driver.findElement(By.xpath("(//span[.='EVA Lite Sport Shoes Running Shoe for Men- Blue'])[1]")).click();
+	Set<String> parentandchildid=driver.getWindowHandles();
+	Iterator <String> it=parentandchildid.iterator();
+	String parentid=it.next();
+	String childid=it.next();
+	driver.switchTo().window(childid);
+	driver.findElement(By.xpath("//a[.=' Add to Wish List ']")).click();
+	
+	
+
+	}
+
+}
